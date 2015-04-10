d11d88f0437721cd3b4cfeb51190f928c157abef
Stephen Levine <stephen.levine@gmail.com>
2015-04-10 00:16:47 -0700

    Fix typo

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Infuse
 Infuse provides an immutable, concurrency-safe middleware handler
 that conforms to http.Handler. An infuse.Handler is fully compatible with
 the standard library, supports flexible chaining, and provides a shared
-context between middleware handlers within a single requests without
+context between middleware handlers within a single request without
 relying on global state, locks, or closures.
--- handler.go
+++ handler.go
@@ -1,7 +1,7 @@
 // Package infuse provides an immutable, concurrency-safe middleware handler
 // that conforms to http.Handler. An infuse.Handler is fully compatible with
 // the standard library, supports flexible chaining, and provides a shared
-// context between middleware handlers within a single requests without
+// context between middleware handlers within a single request without
 // relying on global state, locks, or closures.
 package infuse
 
