36351ee2b100644048dfcdb86463a306ec797562
Stephen Levine <stephen.levine@gmail.com>
2015-04-10 19:28:44 -0700

    More documentation updates

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ that conforms to `http.Handler`. An `infuse.Handler` is fully compatible with
 the standard library, supports flexible chaining, and provides a shared
 context between middleware handlers within a single request without
 relying on global state, locks, or closures.
+
--- handler.go
+++ handler.go
@@ -2,17 +2,14 @@
 // that conforms to http.Handler. An infuse.Handler is fully compatible with
 // the standard library, supports flexible chaining, and provides a shared
 // context between middleware handlers within a single request without
-// relying on global state, locks, or closures.
+// relying on global state, locks, or shared closures.
 package infuse
 
 import "net/http"
 
-// A Handler is an immutable, thread-safe middleware handler that conforms to
-// http.Handler. Handlers are fully compatible with the standard library,
-// support flexible chaining, and provide shared contexts for individual
-// requests without relying on global state, locks, or closures.
+// A Handler is a middleware handler that conforms to http.Handler.
 //
-// An http.Handler passed to the Handle methods can call the http.Handler
+// An http.Handler passed to the Handle method can call the http.Handler
 // provided immediately after it using infuse.Next. All handlers within a
 // single request-response cycle can share data of any type using infuse.Get
 // and infuse.Set.
@@ -21,7 +18,7 @@ type Handler interface {
 	// http.Handler after all existing http.Handlers are served. The
 	// newly-provided http.Handler will be called when the previously-provided
 	// http.Handler calls infuse.Next. The provided http.Handler may be
-	// an infuse.Handler.
+	// an infuse.Handler itself.
 	Handle(handler http.Handler) Handler
 
 	// HandleFunc is the same as Handle, but it takes a handler function
@@ -78,9 +75,10 @@ type contextualResponse struct {
 	layers  []*layer
 }
 
-// Next should only be called from within an http.Handler that is being served
-// by an infuse.Handler. It will serve the next http.Handler in the middleware
-// stack. Next returns false if no subsequent http.Handler is available, or if
+// Next should only be called from within an http.Handler that is served by an
+// infuse.Handler. Next serves the next http.Handler in the middleware stack.
+//
+// Next returns false if no subsequent http.Handler is available, or if
 // the provided response object was not served by an infuse.Handler. Calling
 // Next multiple times in the same handler will run the rest of the middleware
 // stack for each call.
